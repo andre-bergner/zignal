6ff3103a2015a48dc0ad49e796ae4da3014ea020
André Bergner <andre.bergner.0@googlemail.com>
2015-11-11 22:23:53 +0100

    (ノಠ益ಠ)ノ彡

---
+++ experimental_steps/demangle.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <cxxabi.h>
+
+inline std::string demangle(const char* name)
+{
+
+    int status = -4;
+
+    char* res = abi::__cxa_demangle(name, NULL, NULL, &status);
+
+    const char* const demangled_name = (status==0)?res:name;
+
+    std::string ret_val(demangled_name);
+
+    free(res);
+
+    return ret_val;
+}
--- experimental_steps/lambda_with_delay.cpp
+++ experimental_steps/lambda_with_delay.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <array>
+#include <vector>
 #include <boost/mpl/int.hpp>
 #include <boost/mpl/min_max.hpp>
 #include <boost/mpl/eval_if.hpp>
@@ -84,6 +85,11 @@ template< typename I >  struct placeholder       { using arity = I; };
 template< typename T >  struct placeholder_arity { using type = typename T::arity; };
 
 
+
+BOOST_PROTO_DEFINE_ENV_VAR( current_input_t, current_input );
+BOOST_PROTO_DEFINE_ENV_VAR( delayed_input_t, delayed_input );
+
+
 namespace transforms
 {
     using namespace proto;
@@ -149,12 +155,11 @@ namespace transforms
 
     struct place_delay : callable_decltype
     {
-        template< typename I , typename J , typename State >
-        auto operator()( placeholder<I> const & , placeholder<J> const & , State const & state ) const
+        template< typename I , typename J , typename Delayed_input >
+        auto operator()( placeholder<I> const & , placeholder<J> const & , Delayed_input const & del_in ) const
         {
-            auto const & s = std::get<I::value-1>(state);
+            auto const & s = std::get<I::value-1>(del_in);
             return s[s.size()-J::value];
-            //return state[state.size()-J::value];
         }
     };
 
@@ -162,11 +167,11 @@ namespace transforms
     struct eval_it : or_
     <   when
         <   delayed_placeholder<>
-        ,   place_delay( _value(_left) , _value(_right) , _state )
+        ,   place_delay( _value(_left) , _value(_right) , _env_var<delayed_input_t> )
         >
     ,   when
         <   terminal< placeholder<_> >
-        ,   place_the_holder( _value , _state )
+        ,   place_the_holder( _value , _env_var<current_input_t> )
         >
     ,   when
         <   _
@@ -205,6 +210,17 @@ struct delay_for_placeholder<0,Expr>
 
 
 
+
+//  ------------------------------------------------------------------------------------------------
+// supporting tools
+//  ------------------------------------------------------------------------------------------------
+
+
+
+//  ------------------------------------------------------------------------------------------------
+// lift_into_tuple  --  lifts a meta-function into a tuple,
+//                      i.e. applies it on each type in tuple, returns tuple of new types
+
 template< typename F , typename Tuple >
 struct lift_into_tuple;
 
@@ -218,6 +234,9 @@ template< typename F , typename Tuple >
 using lift_into_tuple_t = typename lift_into_tuple<F,Tuple>::type;
 
 
+
+//  ------------------------------------------------------------------------------------------------
+// to_array  --  meta-function that maps mpl::int_<N> to array<T,N>
 
 template< typename T >
 struct to_array
@@ -234,10 +253,8 @@ struct to_array
 
 
 
-
-
-
-
+//  ------------------------------------------------------------------------------------------------
+// tuple_for_each  --  apply n-ary function on each value in list of n tuples of same size
 
 template< size_t N , size_t n = 0 >
 struct tuple_for_each_impl
@@ -259,11 +276,15 @@ struct tuple_for_each_impl<N,N>
 
 
 template< typename F, typename... Ts , typename... Tuples >
+// requires (sizeof...(Ts) == tuple_size(Tuples))...
 void tuple_for_each( F&& f , std::tuple<Ts...>& t , Tuples&&... tuples )
 {
     tuple_for_each_impl< sizeof...(Ts) >::apply( std::forward<F>(f), t, std::forward<Tuples>(tuples)... );
 }
 
+
+//  ------------------------------------------------------------------------------------------------
+// very simple delay_line operation on std::array --> TODO should be own type static_delay_line !
 
 struct {
     template< typename T , size_t N , typename Y >
@@ -276,20 +297,94 @@ struct {
 
 
 
+
+//  ------------------------------------------------------------------------------------------------
+// compile  --  main function of framework
+//              • takes a zignal/dsp expressions
+//              • generta
+//              • returns clojure
+//  ------------------------------------------------------------------------------------------------
+
 auto compile = []( auto expr )        // TODO need to define value_type for state
 {
     using expr_t = decltype(expr);
     using tuple_t = typename delay_for_placeholder< lambda_arity_t<expr_t>::value, expr_t >::type;
     using state_t = lift_into_tuple_t< to_array<float> , tuple_t >;
 
-    return [ expr , state = state_t{} ]( auto&&... xs ) mutable
+    return [ expr , state = state_t{} ]( auto const&... xs ) mutable
     {
-        auto result = eval_it{}( expr, state );
-        tuple_for_each( rotate_push_back, state, std::forward_as_tuple(xs...) );
-        //rotate_push_back( state, std::forward<decltype(xs)>(xs)... );
+        auto result = eval_it{}( expr, proto::_state{}, ( current_input = std::make_tuple(xs...) , delayed_input = state ) );
+        tuple_for_each( rotate_push_back, state, std::make_tuple(xs...) );
         return result;
     };
 };
+
+
+
+//  ------------------------------------------------------------------------------------------------
+// compile2 -- same as compile, but return type is curryable
+//  ------------------------------------------------------------------------------------------------
+
+template
+<   typename Expression
+,   typename State
+,   size_t   arity
+>
+struct stateful_lambda
+{
+private:
+
+    Expression  expr_;
+    State       state_;
+
+    template< typename... Args >
+    auto call_impl( mpl::int_<0> , Args const &... args ) -> decltype(auto)
+    {
+        auto result = eval_it{}( expr_, proto::_state{}, ( current_input = std::make_tuple(args...) , delayed_input = state_ ) );
+        tuple_for_each( rotate_push_back, state_, std::make_tuple(args...) );
+        return result;
+    }
+
+    template< int arg_diff , typename... Args >
+    auto call_impl( mpl::int_<arg_diff> , Args const &... args ) const
+    {
+        return [ &args...                 // TODO should copy args into lambda
+               , expr = *this ]           // TODO should not be a lambda, but a type that is
+        ( auto const &... missing_args )  //      aware of the # of args (enable_if them)
+        {
+            return expr( args..., missing_args... );
+        };
+    }
+
+public:
+
+    stateful_lambda( Expression expr ) : expr_( std::move(expr)) {}
+
+    template< typename... Args , typename = std::enable_if_t< sizeof...(Args) <= arity > >
+    auto operator()( Args const &... args ) -> decltype(auto)
+    {
+        return call_impl( mpl::int_< arity - sizeof...(Args) >{} , args... );
+    }
+};
+
+
+auto compile2 = []( auto expr )        // TODO need to define value_type for state
+{
+    using expr_t = decltype(expr);
+    using arity_t = lambda_arity_t<expr_t>;
+    using tuple_t = typename delay_for_placeholder< arity_t::value, expr_t >::type;
+    using state_t = lift_into_tuple_t< to_array<float> , tuple_t >;
+
+    return stateful_lambda< expr_t, state_t, arity_t::value>{ expr };
+};
+
+
+
+
+
+
+
+
 
 
 const proto::terminal< placeholder< mpl::int_<1> >>::type   _1  = {{}};
@@ -327,11 +422,30 @@ int main()
 
 
     auto unit_delay = compile( _1[_1] );
-    auto differntiator = compile( _1[_1] - _1[_2] );
+    auto differentiator = compile2( _1 - _1[_1] );
 
     for ( auto x : {1,1,1,1,1} )
-        std::cout << differntiator(x) << std::endl;
-//    for ( auto x : {1,2,3,4,5,6,7,8,9} )
-//        std::cout << differntiator(x,x) << std::endl;
+        std::cout << differentiator(x) << std::endl;
 
+/*
+    {  // test to check assembly output
+        auto test_expr = compile( _1[_1] - _1[_2]*2.f );
+
+        std::vector<float> xs(1000);
+        {
+            float u = 0.87;
+            for ( auto& x : xs ) x = u *= 4.0*(1.-u);
+        }
+
+        auto acc = 0.f;
+        __asm__ __volatile__("nop":::"memory");
+        for ( auto x : xs )
+            acc += test_expr(x);
+        __asm__ __volatile__("nop":::"memory");
+        std::cout << acc << std::endl;
+
+        for ( auto x : xs )
+            std::cout << test_expr(x) << std::endl;
+    }
+*/
 }
