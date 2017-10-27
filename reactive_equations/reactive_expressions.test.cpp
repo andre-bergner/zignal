bad39bc6977922cb0adbb06c5da140ecefa81da6
André Bergner <andre.bergner.0@gmail.com>
2017-10-27 14:28:21 +0200

    Small unit test for reactive expressions.

--- reactive_equations/reactive_expressions.test.cpp
+++ reactive_equations/reactive_expressions.test.cpp
@@ -3,7 +3,6 @@
 
 #include "reactive_expressions.hpp"
 
-
 int main()
 {
    PARAMETER( float, s1, 12 );
@@ -13,35 +12,23 @@ int main()
    PARAMETER( float, d3, 0 );
 
    auto deps = make_mapping_dag
-   (  s1   = 12
-   ,  s2   = -3
-   ,  d1   = 1.f / (s1*s1 + s2*s2)
-   ,  d2   = s1 * d1
-   ,  d3   = s1 * d2
+   (  s1  = 12
+   ,  s2  = -3
+   ,  d1  = 1.f / (s1*s1 + s2*s2)
+   ,  d2  = s1 * d1
+   ,  d3  = s1 * d2
    );
 
-   auto print_params = [&]{
-      std::cout << "----------------------------------" << std::endl;
-      std::cout << "s1:  " << deps.get(s1) << std::endl;
-      std::cout << "s2:  " << deps.get(s2) << std::endl;
-      std::cout << "d1:  " << deps.get(d1) << std::endl;
-      std::cout << "d2:  " << deps.get(d2) << std::endl;
-      std::cout << "d3:  " << deps.get(d3) << std::endl;
-      std::cout << "----------------------------------" << std::endl;
-   };
-
-   print_params();
+   assert( deps.get(s1) == 12.f );
+   assert( deps.get(s2) == -3.f );
+   assert( deps.get(d1) == 1.f / 153.f );
+   assert( deps.get(d2) == (1.f / 153.f) * 12.f );
+   assert( deps.get(d3) == (1.f / 153.f) * 12.f * 12.f );
 
    deps.set(s2, 0);
-   print_params();
-/*
-   std::cout << "set a=10." << std::endl;
-   print_params();
-
-   std::cout << "set a=-23, b=1337." << std::endl;
-   deps.set(a, -23);
-   deps.set(b, 1337);
-   print_params();
-*/
+   assert( deps.get(s1) == 12.f );
+   assert( deps.get(s2) == 0.f );
+   assert( deps.get(d1) == 1.f / 144.f );
+   assert( deps.get(d2) == (1.f / 144.f) * 12.f );
+   assert( deps.get(d3) == 1.f );
 }
-
